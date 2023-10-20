62a1468733e02aff4f75653bf95949d783633d9d
audry <kmkyou1@nate.com>
2023-10-20 20:48:45 +0900

    수정

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -2,7 +2,7 @@ let skill = document.querySelector(".skill");
 let x = true;
 let i = document.querySelectorAll(".skill ul li i");
 let t = document.querySelectorAll(".skill ul li .per");
-const per = [85, 85, 90, 85, 50, 50]
+const per = [90, 90, 90, 85, 50, 50]
 
 window.addEventListener("scroll", function (e) {
 	console.log(skill.getBoundingClientRect().top)
