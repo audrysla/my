a8b5a6bf2fc514dada844e6854800ba3333df87a
audry <kmkyou1@nate.com>
2023-10-21 03:03:34 +0900

    스크롤 이동 기능 적용

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -1,11 +1,15 @@
-let skill = document.querySelector(".skill");
+const skill = document.querySelector(".skill");
+const about = document.querySelector(".about");
+const header = document.querySelector("header");
+const nav = document.querySelectorAll("nav a");
+const article = document.querySelectorAll("article");
+const per = [90, 90, 90, 85, 50, 50]
 let x = true;
 let i = document.querySelectorAll(".skill ul li i");
 let t = document.querySelectorAll(".skill ul li .per");
-const per = [90, 90, 90, 85, 50, 50]
 
 window.addEventListener("scroll", function (e) {
-	console.log(skill.getBoundingClientRect().top)
+	// console.log(skill.getBoundingClientRect().top)
 	if(skill.getBoundingClientRect().top <= window.innerHeight){
 		if(x){
 			i.forEach((item, index) =>{
@@ -15,6 +19,23 @@ window.addEventListener("scroll", function (e) {
 		}
 		x = false;
 	}
+	about.getBoundingClientRect().top < 0+header.clientHeight ? header.classList.add('bg') : header.classList.remove('bg');
 });
 
+nav.forEach((item, index) =>{
+	item.addEventListener('click', function(){
+		window.scrollTo({
+			top : article[index+1].offsetTop-header.clientHeight,
+			left : 0,
+			behavior: 'smooth'
+		});
+	});
+});
 
+document.querySelector("header h1 a").addEventListener('click',() =>{
+	window.scrollTo({
+		top : 0,
+		left : 0,
+		behavior: 'smooth'
+	});
+});
