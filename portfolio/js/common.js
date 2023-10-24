0b23d3423846f19581a320aa809c1debaa3c07a2
audry <kmkyou1@nate.com>
2023-10-24 15:07:47 +0900

    js

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -6,6 +6,7 @@ const nav = document.querySelectorAll("nav a");
 const skill = document.querySelector(".skill");
 const about = document.querySelector(".about");
 // skill
+const dogBone = document.querySelector(".parallax-div .material-symbols-outlined");
 const per = [90, 90, 70, 90, 90, 50, 50]
 let x = true;
 let i = document.querySelectorAll(".skill ul li i");
@@ -45,7 +46,10 @@ document.querySelector("header h1 a").addEventListener('click',() =>{
 
 window.addEventListener("scroll", function (e) {
 	// (window.scrollY > 100) ? main.classList.add("static") : main.classList.remove("static");	// 메인 풀화면
-	if(skill.getBoundingClientRect().top <= winHi){		
+	if(skill.getBoundingClientRect().top <= winHi && skill.getBoundingClientRect().top + skill.clientHeight > 0){
+		// console.log(skill.getBoundingClientRect().top)
+		dogBone.style.willChange = 'transform'; 
+		console.log(dogBone.style.willChange)
 		parallaxEv.init();
 		parallaxEv.action();
 		if(x){
@@ -56,6 +60,10 @@ window.addEventListener("scroll", function (e) {
 			});
 		}
 		x = false;
+	}
+	if(skill.getBoundingClientRect().top + skill.clientHeight < 0){
+		dogBone.style.willChange = 'auto';
+		console.log(dogBone.style.willChange)
 	}
 	// 스크롤 내려갈 때 헤더 백그라운드 주기
 	about.getBoundingClientRect().top < 0+header.clientHeight ? header.classList.add('bg') : header.classList.remove('bg');
