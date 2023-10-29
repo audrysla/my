b0d8b9db520dcd435f3c04f3424fd33fcdcc4b12
audry <kmkyou1@nate.com>
2023-10-30 00:13:48 +0900

    css

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -103,8 +103,9 @@ window.addEventListener("scroll", function (e) {
 
 	// 스킬 영역 지나면 고정
 	if(parallaxArea <= 0 ){
+		document.querySelector('body').classList.add('bg');
 		backboard.classList.add('fixed');									// 페럴렉스 영역 고정
-		parallax.querySelector('.backboard').style.height = `${winHi+100}px`	// 높이값 화면 높이로 지정(풀화면)
+		parallax.querySelector('.backboard').style.height = `${winHi}px`	// 높이값 화면 높이로 지정(풀화면)
 	
 		let scrollTop = moveScroll * 0.002;
 		let transformSpeed = 30;
@@ -173,6 +174,7 @@ window.addEventListener("scroll", function (e) {
 			t3.style.opacity = `0`
 		}
 	}else{
+		document.querySelector('body').classList.remove('bg');
 		backboard.classList.remove('fixed');
 		t1.style.opacity = `0`;
 		t2.style.opacity = `0`;
